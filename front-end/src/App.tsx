67e3d89c94f1a46338d0a2a4ae5ac1f67cd3c2bf
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2020-08-08 15:28:36 -0300

    Assets and global styles

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './styles/global.css'
+
 function App() {
   return (
     <div>
-      <h1>Hello proffy</h1>
+      <h1>Hello proffys</h1>
     </div>
   );
 }
